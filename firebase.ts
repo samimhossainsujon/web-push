465acf65ff410e2e6221ce491ef76bfd2c9580cf
Md . Samim Hossain Sujon <98223435+samimhossainsujon@users.noreply.github.com>
2024-12-21 13:36:40 +0600

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,13 @@ import { getMessaging, getToken, isSupported } from "firebase/messaging";
 
 // Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAcmymWqW0BNeWMHVW1Z2lsvBvVu5tcJpY",
-  authDomain: "job-circular-net.firebaseapp.com",
-  databaseURL: "https://job-circular-net-default-rtdb.firebaseio.com",
-  projectId: "job-circular-net",
-  storageBucket: "job-circular-net.appspot.com",
-  messagingSenderId: "872223083121",
-  appId: "1:872223083121:web:3561221037e1466c5429d0",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
